c0974f4371448738c426d62551ffacf42cb09831
HollowTube <truongtritin98@gmail.comgit config --global user.email truongtritin98@gmail.com>
2017-10-03 23:05:33 -0400

    Quicksort finished, needs pointer implementation

--- Quicksort.c
+++ Quicksort.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-int num[10] = {5,8,6,1,9,0,2,3,4,7};
+int num[10] = {15,98,160,1341,69,12,12,16,14,23};
 int swap(int a, int b, int arr[]){
   int temp = arr[a];
   arr[a] = arr[b];
@@ -12,25 +12,49 @@ int swap(int a, int b, int arr[]){
 int pivot(int left, int right, int arr[]){
   int pivot  = arr[left]; //pivot value defined as first element in partition
   int i = left; //storing location of last swap, also tracks the number of elements below partition
-  for (int k = left + 1; k< right; k++ ){
-    printf("arr[k] = %d, i = %d\n", arr[k], i);
-    if(arr[k] < pivot){
+  for (int k = left + 1; k<=right; k++ ){
+    // printf("arr[k] = %d, i = %d\n", arr[k], i);
+    if(arr[k] <= pivot){
       i++;
       swap(i,k,arr);
-      printf("Swap\n" );
-      for (int i=0; i < 10; i ++){
-        printf("%d", num[i]);
-      }
-      printf("\n");
+      //printf("Swap\n" );
+      // for (int i=0; i < 10; i ++){
+      //   printf("%d", num[i]);
+      // }
+      // printf("\n");
     }
   }
   swap(left, i, arr);
-  return 1;
+  for (int i=0; i < 10; i ++){
+    printf("%d ", num[i]);
+  }
+  printf("\n");
+  return  i;
 }
+int quickSort(int left, int right, int arr[]){
+  int npivot = pivot(left, right, arr);
+  printf("left = %d, right  = %d, pivotIndex  = %d \n",left, right, npivot);
+    if (left > right){
+      printf("moving up stack\n");
+    }
+  if (left < right)
+  {
+      /* pi is partitioning index, arr[p] is now
+         at right place */
+
+
+
+      // Separately sort elements before
+      // partition and after partition
+      quickSort(left, npivot - 1, arr);
+      quickSort(npivot + 1, right, arr);
+  }
+}
+
 
 int main(int argc, char const *argv[]) {
 
-  pivot(0,10,num);
+  quickSort(0,9, num);
   for (int i=0; i < 10; i ++){
     printf("\n ");
     printf("%d ", num[i]);
